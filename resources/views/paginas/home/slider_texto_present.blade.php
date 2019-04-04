f937d2cebda03e73b16b34ba11e8a9e9747c5859
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-04-03 22:34:48 -0300

    fg

--- resources/views/paginas/home/slider_texto_present.blade.php
+++ resources/views/paginas/home/slider_texto_present.blade.php
@@ -1,6 +1,6 @@
  <div class="col-lg-10 mx-auto">
       <h1 class="text-uppercase Slider_cabecera_caption_titulo text-color-black">
-        <strong>¿Buscas una web ecónomica pero bien diseñada?</strong>
+        <strong>¿Buscas joyas de calidad y económicas?</strong>
       </h1>
       
 </div>
@@ -8,8 +8,8 @@
 <div class="col-lg-8 mx-auto">
   <h2 class="text-faded mb-5 Slider_cabecera_caption_sub_titulo text-color-primary">Tenemos la que necesitas.</h2>
  
-  <a class="btn btn-primary btn-xl js-scroll-trigger Slider_cabecera_boton_contacto" href="#precios">Ver planes</a>
-   <p class="text-faded my-4 text-color-primary" style="font-style:italic; font-weight: bold;">#WEBSDECALIDAD</p>
+  <a class="btn btn-primary btn-xl js-scroll-trigger Slider_cabecera_boton_contacto" href="#precios">Ver precios</a>
+   {{-- <p class="text-faded my-4 text-color-primary" style="font-style:italic; font-weight: bold;">#WEBSDECALIDAD</p> --}}
 </div>
          
 
