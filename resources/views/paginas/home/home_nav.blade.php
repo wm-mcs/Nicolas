2f3da42faebfe2f72d5b46a4880508d46e1afae5
Mauricio Costanzo <ventas@worldmaster.com.uy>
2019-04-24 13:48:10 -0300

    ok

--- resources/views/paginas/home/home_nav.blade.php
+++ resources/views/paginas/home/home_nav.blade.php
@@ -4,7 +4,13 @@
             </li> 
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger " href="#precios">CATALOGO</a>
-            </li>            
+            </li>  
+             <li class="nav-item">
+              <a class="nav-link " href="https://nicolas.worldmaster.com.uy/Publicacion/Significado-de-las-Piedras-Energ%C3%A9ticas-m%C3%A1s-utilizadas-en-joyer%C3%ADa/1">SIGNIFICADOS</a>
+            </li> 
+             <li class="nav-item">
+              <a class="nav-link " href="https://nicolas.worldmaster.com.uy/Publicacion/Como-cuidar-una-joya/2">CUIDADOS</a>
+            </li>           
            
                       
             <li class="nav-item">
--- resources/views/paginas/home/home_piedras_cuidados.blade.php
+++ resources/views/paginas/home/home_piedras_cuidados.blade.php
@@ -16,3 +16,34 @@
           </a>
       </div>
 </section>
+
+
+ <section class="bg-primary py-8">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading text-white">¿Cómo cuidar una joya? </h2>
+           
+            <hr class="light my-4">
+            <p class="text-white mb-4">Muchas piezas pueden tener mucho más que un mero valor
+económico y recordarnos a alguien o a un momento especial en nuestras vidas.
+Es por ello que esa alaha es única para nosotros y su pérdida nos causaría mucha pena. Por eso
+he aquí una serie de consejos y trucos, muchos de los cuales usted puede realizar en su hogar
+para aportar a su pieza un periodo de vida mayor y en mejores condiciones.
+Presta atención a estos sencillos consejos que pueden hacer ver a tu joya siempre nueva y
+espectacular
+            </p>
+
+        
+            
+          </div>
+        </div>
+       <div class="get_width_100 flex-row-column">
+            <a class="mt-5" href="https://nicolas.worldmaster.com.uy/Publicacion/Como-cuidar-una-joya/2">
+            <div class="boton-simple-blanco">
+             Quiero ver <i class="far fa-grin"></i>
+            </div>
+          </a>
+        </div>
+      </div>
+    </section>
